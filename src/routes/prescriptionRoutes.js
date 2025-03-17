b85c340a3d6785144b92c34a3f9568143c7a9184
Dmytro Asparian <dima.supckik2004@gmail.com>
2025-03-17 15:09:45 +0200

    Prescriptions in past fix

--- src/routes/prescriptionRoutes.js
+++ src/routes/prescriptionRoutes.js
@@ -70,6 +70,18 @@ router.post("/create", authenticate(["doctor"]), async (req, res) => {
         .json({ message: "Діагноз і лікування є обов'язковими." });
     }
 
+    if (validUntil) {
+      const validUntilDate = new Date(validUntil);
+      const today = new Date();
+      today.setHours(0, 0, 0, 0);
+
+      if (validUntilDate < today) {
+        return res.status(400).json({
+          message: "Дата закінчення дії рецепту не може бути в минулому.",
+        });
+      }
+    }
+
     // Створюємо призначення
     const newPrescription = new Prescription({
       doctor: doctorId,
