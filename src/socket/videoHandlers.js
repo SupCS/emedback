1f43601e9a570d6f36d684d2e72a6c855af0ad62
Dmytro Asparian <dima.supckik2004@gmail.com>
2025-04-23 10:06:55 +0300

    video room without camera / audio

--- src/socket/videoHandlers.js
+++ src/socket/videoHandlers.js
@@ -1,4 +1,4 @@
-const { roomSockets, socketToRoom } = require("./state");
+const { roomSockets, socketToRoom, users } = require("./state");
 
 module.exports = (socket, io) => {
   socket.on("join-room", ({ callId }) => {
@@ -47,5 +47,21 @@ module.exports = (socket, io) => {
     const participants = roomSockets.get(callId);
     const other = [...participants].find((id) => id !== socket.id);
     if (other) io.to(other).emit("ice-candidate", { candidate });
+  });
+
+  socket.on("disconnect", () => {
+    console.log(`🔴 User ${socket.user?.id} disconnected`);
+    if (socket.user?.id) users.delete(socket.user.id);
+
+    for (const [callId, sockets] of roomSockets.entries()) {
+      if (sockets.has(socket.id)) {
+        sockets.delete(socket.id);
+        socketToRoom.delete(socket.id);
+        socket.to(callId).emit("user-left", { socketId: socket.id });
+        if (sockets.size === 0) {
+          roomSockets.delete(callId);
+        }
+      }
+    }
   });
 };
