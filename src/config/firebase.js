88918a5778c960188415dd75d8f775fcbc4d9361
Dmytro Asparian <dima.supckik2004@gmail.com>
2025-04-29 11:36:27 +0300

    Update firebase config

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,24 +1,13 @@
 const admin = require("firebase-admin");
 
-// Якщо GOOGLE_CREDENTIALS_JSON є в середовищі — парсимо її
-if (process.env.GOOGLE_CREDENTIALS_JSON) {
-  admin.initializeApp({
-    credential: admin.credential.cert(
-      JSON.parse(process.env.GOOGLE_CREDENTIALS_JSON)
-    ),
-    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  });
-} else {
-  const path = require("path");
-  const serviceAccountPath = path.resolve(
-    __dirname,
-    "emed-video-firebase-adminsdk-fbsvc-32da974313.json"
-  );
-  admin.initializeApp({
-    credential: admin.credential.cert(require(serviceAccountPath)),
-    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  });
-}
+admin.initializeApp({
+  credential: admin.credential.cert({
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
+    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+  }),
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+});
 
 const db = admin.firestore();
 const storage = admin.storage().bucket();
