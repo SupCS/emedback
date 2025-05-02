0615ef9629f06760b2c6f4a073927d92c2df02a8
Dmytro Asparian <dima.supckik2004@gmail.com>
2025-05-02 20:19:08 +0300

    Update backend url in swagger cfg

--- src/config/swagger.js
+++ src/config/swagger.js
@@ -2,26 +2,26 @@ const swaggerJsDoc = require("swagger-jsdoc");
 const swaggerUi = require("swagger-ui-express");
 
 const options = {
-    definition: {
-        openapi: "3.0.0",
-        info: {
-            title: "EMed API Documentation",
-            version: "1.0.0",
-            description: "Документація для API платформи EMed",
-        },
-        servers: [
-            {
-                url: "http://localhost:5000", // Базовий URL бекенду
-            },
-        ],
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "EMed API Documentation",
+      version: "1.0.0",
+      description: "Документація для API платформи EMed",
     },
-    apis: ["./src/routes/*.js"], // Шлях до всіх файлів з маршрутами
+    servers: [
+      {
+        url: "https://emed-backend-fc35c553180b.herokuapp.com", // Базовий URL бекенду
+      },
+    ],
+  },
+  apis: ["./src/routes/*.js"], // Шлях до всіх файлів з маршрутами
 };
 
 const swaggerSpec = swaggerJsDoc(options);
 
 const swaggerDocs = (app) => {
-    app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+  app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 };
 
 module.exports = swaggerDocs;
